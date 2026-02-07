33ce89dec48f22c7b756224bca6582bbe4752248
player-001 <mohitworksop@gmail.com>
2026-02-07 17:04:01 -0500

    updated

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -52,7 +52,7 @@ function searchAndDisplay(){
 function resetSearch(){
     var result = document.getElementById("result")
     result.innerHTML = ""
-    document.getElementById("search").value = "a"
+    document.getElementById("search").value = ""
 }
 searchBtn.addEventListener("click", searchAndDisplay)
 reset.addEventListener("click", resetSearch)
