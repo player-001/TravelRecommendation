2fb13cb85017197e03c7ecc5a87bb4a7516d81f7
player-001 <mohitworksop@gmail.com>
2026-02-07 08:11:24 -0500

    added

---
+++ travel_recommendation.js
@@ -0,0 +1,10 @@
+const search = document.getElementById("search")
+const searchBtn = document.getElementById("searchBtn")
+const reset = document.setElementById("reset")
+const submit = document.getElementById("submit")
+
+function searchAndDisplay(){
+    fetch("./travel_recommendation_api.json")
+        .then()
+}
+search.addEventListener("click", searchAndDisplay())
